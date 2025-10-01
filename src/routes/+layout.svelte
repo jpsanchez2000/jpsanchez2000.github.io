bd0eaf6336240b6d24f4e218066fb3817a9bcade
Jude Sanchez <judesanchez2000@gmail.com>
2025-10-01 00:09:10 -0600

    added theme and basic ui start

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,7 @@
 </script>
 
 <svelte:head>
+	<title>Jude Sanchez</title>
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,32 @@
-<div class="bg-blue-600">hello world</div>
+<script>
+	/**
+	 * @type {any[]}
+	 */
+	let inputList = [];
+	let typedValue = '';
+
+	$: console.log(typedValue, inputList);
+</script>
+
+<div class="h-full w-full bg-surface-900 p-2">
+	<div class="m-0.5 flex h-full w-full flex-col-reverse rounded-2xl border border-primary-600 p-2">
+		<div class="flex">
+			<div>~/Portfolio %</div>
+			<input
+				class="h-6 border-0 bg-surface-900 text-base focus:ring-0 focus:outline-none"
+				autofocus
+				bind:value={typedValue}
+				on:keypress={(key) => {
+					console.log(key.key);
+					if (key.key === 'Enter') {
+						inputList = [...inputList, typedValue];
+						typedValue = '';
+					}
+				}}
+			/>
+		</div>
+		{#each inputList as prevInput}
+			<div>{prevInput}</div>
+		{/each}
+	</div>
+</div>
